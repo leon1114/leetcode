4d03f98a21affef17ec22d2c1f0931b04b2de9f0
Leon Ryu <leon1114@naver.com>
2022-06-12 21:12:19 +0900

    added weekly 0612 problem3 solution

--- contests/weekly_0612/problem_3.py
+++ contests/weekly_0612/problem_3.py
@@ -3,13 +3,46 @@ from typing import *
 
 # https://leetcode.com/problems/fair-distribution-of-cookies/
 
+import math
 class Solution:
+    """
+    Runtime: 90 ms, faster than 100.00% of Python3 online submissions for Fair Distribution of Cookies.
+    Memory Usage: 13.9 MB, less than 37.50% of Python3 online submissions for Fair Distribution of Cookies.
+    """
+    def bt(self, cookie_idx: int):
+        """
+        cookie_idx 에 해당하는 cookie 를 하나의 bag 에 넣고, 다음 cookie_idx 대상으로 bt 호출
+
+
+        """
+        # 종료조건 설정
+        if max(self.bags) >= self.min_max:
+            return
+
+        if cookie_idx == len(self.cookies):
+            max_value = max(self.bags)
+            if max_value < self.min_max:
+                self.min_max = max_value
+            return 
+
+        # 각 가방 하나씩 순회하면서 쿠키를 넣어봄. 
+        for idx in range(len(self.bags)):
+            self.bags[idx] += self.cookies[cookie_idx]
+            self.bt(cookie_idx+1)
+            self.bags[idx] -= self.cookies[cookie_idx]
+
+
     def distributeCookies(self, cookies: List[int], k: int) -> int:
         """
         cookies list 에 있는 element 들을 k 개로 bagging 하는데, 들어있는 값들의 합이 최대인 bag 가 최소의 합을 갖도록 해야 함. 
         back tracking?
         """
-        pass
+
+        self.bags = [0] * k
+        self.cookies = cookies
+        self.min_max = math.inf
+        self.bt(0)
+        return self.min_max
 
 
 class SolutionTest(unittest.TestCase):
